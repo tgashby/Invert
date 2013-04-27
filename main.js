88b3ae721db1ae7273b1222f9d58b510334e8f4b
Tag Ashby <tgashby@calpoly.edu>
2013-04-27 07:17:53 -0700

    Added background music

--- main.js
+++ main.js
@@ -5,8 +5,15 @@ var gameHeight = 500;
 var gravity = 1;
 var player;
 var upBlockHeight=400;
+<<<<<<< HEAD
 var downBlockHeight = 500;
 var gameStateUp = false;
+=======
+
+var assets = ['player.png', 'platform.png', 
+              'backgroundCube.png', 'CutRunDownLoop.ogg', 
+              'CutRunLoop.ogg', 'CutRunTransition.ogg'];
+>>>>>>> Added background music
 
 Player = Class.create(Sprite, {
 	initialize: function () {
@@ -16,26 +23,34 @@ Player = Class.create(Sprite, {
 		this.x = gameWidth / 2 - this.width / 2;
 		this.y = gameHeight - this.height * 2;
       
-      this.globalY = this.y;
-		this.vel = {x:5, y:25};
+    this.globalY = this.y;
+		this.vel = {x:10, y:25};
 	},
 
 	onenterframe: function() {
 		this.globalY -= this.vel.y;
-      this.vel.y -= gravity;
+    this.vel.y -= gravity;
       
 		if (game.input.left)
 			this.x -= this.vel.x;
 
 		if (game.input.right)
 			this.x += this.vel.x;
+
 	  this.y = this.globalY-camera.globalY;
+
+    if (this.x + this.width < 0)
+      this.x = gameWidth;
+
+    if (this.x > gameWidth)
+      this.x = 0 - this.width;
    }
 });
 
 
 window.onload = function() {
     game = new Game(gameWidth, gameHeight);
+<<<<<<< HEAD
     game.preload('player.png', 'platform.png', 'backgroundCube.png' );
     game.onload = function() {
       game.fps=120;
@@ -45,10 +60,25 @@ window.onload = function() {
 		game.keybind(83, 'down');
       //game.rootScene.backgroundColor='black';
     	player = new Player()
+=======
+    game.preload(assets);
+
+    game.onload = function() {
+      game.fps=120;
+    	game.keybind(65, 'left');
+  		game.keybind(68, 'right');
+  		game.keybind(87, 'up');
+  		game.keybind(83, 'down');
+
+    	player = new Player();
+>>>>>>> Added background music
       camera = new Camera(player);
+
     	game.rootScene.addChild(player);
       game.rootScene.addChild(new Platform(0,gameHeight-100,gameWidth));
+
       game.rootScene.addEventListener('enterframe',function(e){
+<<<<<<< HEAD
          camera.update();
          if (Math.random() * 1000 < 30)
             game.rootScene.addChild(new Rotating());
@@ -69,8 +99,23 @@ window.onload = function() {
             } 
       }
       
+=======
+        camera.update();
+        if (Math.random() * 1000 < 10)
+           game.rootScene.addChild(new Rotating());
+
+        while(upBlockHeight - (camera.globalY - 80) > 0){
+            upBlockHeight = createUpPlatforms(upBlockHeight); 
+            //console.log(upBlockHeight);
+         }
+>>>>>>> Added background music
       });
 
+      game.bgm = game.assets['CutRunLoop.ogg'];
+      game.bgm._element.loop = true;
+        
+      game.bgm.play();
     }
+
     game.start();
 }
